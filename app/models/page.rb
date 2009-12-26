4149340c6ecfb88a1edb49b92a7db01b0cb7b1be
Tad Thorley <tadt@decho.com>
2009-12-25 23:05:49 -0700

    added basic model associations

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,2 +1,7 @@
 class Page < ActiveRecord::Base
+  belongs_to :book
+  belongs_to :author
+  
+  has_many :child_pages, :class_name => "Page"
+  belongs_to :parent_page, :class_name => "Page", :foreign_key => "page_id"
 end
