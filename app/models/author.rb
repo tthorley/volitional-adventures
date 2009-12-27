9e3bcf9ba78fb75ddfc210b27bc34dec4f2c81a8
Tad Thorley <tadt@decho.com>
2009-12-26 20:48:44 -0700

    author passwords/checks

--- app/models/author.rb
+++ app/models/author.rb
@@ -6,10 +6,25 @@ class Author < ActiveRecord::Base
   def password=(pass)
     salt = (0...8).map{65.+(rand(25)).chr}.join
     self.password_salt = salt
-    self.password_hash = Author.hashify(salt + pass) 
+    self.password_hash = Author.hashify(:salt => salt, :password => pass) 
   end
   
-  def self.hashify(string)
-    Digest::SHA256.hexdigest(string)
+  def password
+    raise "password stored as a hash"
+  end
+  
+  def Author.hashify(options)
+    salt = options[:salt] || ""
+    pass = options[:password] || ""
+    Digest::SHA256.hexdigest(pass+salt)
+  end
+  
+  def Author.authenticate(name, password)
+    author = Author.find_by_name(name)
+    if author && Author.hashify(:salt => author.password_salt, :password => password) == author.password_hash
+      author
+    else
+      nil
+    end
   end
 end
